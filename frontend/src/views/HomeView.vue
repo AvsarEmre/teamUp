5f6ec67bacfa0d217cbd3fcb0fd6b9c1abadcd04
EmreAvsar <emre.avsar@hotmail.com>
2022-05-23 16:02:52 +0200

    babel error

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,29 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  .home
+    img (alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
+    div(v-for="user in users") {{ user.name }} has {{ user.teams.length }} teams
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: "HomeView",
+  name: 'HomeView',
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      users: [],
+    }
+  },
+  async created() {
+    const userRequest = await axios.get('/api/users')
+
+    this.users = userRequest.data
   },
 };
 </script>
